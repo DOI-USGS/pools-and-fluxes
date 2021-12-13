28062840713aefd637fedb8c850998d35ae8eb18
collnell <cnell@usgs.gov>
2021-12-13 07:27:55 -0600

    add data procesing

---
+++ _targets.R
@@ -0,0 +1,28 @@
+library(targets)
+library(dssecrets)
+library(tidyverse)
+
+options(tidyverse.quiet = TRUE)
+
+source("data-src/s3_upload.R")
+source("data-src/prep_logpile_data.R")
+
+list(
+  tar_target(
+    abbott_data,
+    read_csv('in/abbott-pools-and-fluxes.csv',
+             skip = 1,
+             col_types = c('ccccnnn'),
+             col_names = c('Type','Category','Feature_group','Feature','Vol_1000km3','Vol_km3', "Vol_m3"))
+  ),
+  tar_target(
+    water_volume,
+    prep_logpile_data(abbott_data, file_out = 'out/water_volume_logpile.csv')
+  ),
+  tar_target(
+    water_volume_log,
+    s3_upload(filepath_s3 = "visualizations/data/abbott_pools_and_fluxes.csv",
+              on_exists = "replace",
+              filepath_local = water_volume)
+  )
+)
