eac406c40679f4c002a2d3cf6ece7f8ae97d2979
collnell <cnell@usgs.gov>
2021-12-27 14:07:07 -0600

    add GA and terms

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="visualization">
-    <h2>The volume of water on Earth</h2>
+    <h2>Pools and Fluxes in the Water Cycle</h2>
     <Logpile />
+    <p>
+      This visualization was inspired by the <a href="https://www.duncangeere.com/carbonincontext/" target="_blank">Carbon in Context</a> logpile chart by Duncan Geere. The magnitude of water cycle components are from <a href="https://www.nature.com/articles/s41561-019-0374-y" target="_blank">Abbott et al. (2019) Human domination of the global water cycle absent from depictions and perceptions.</a>  
+    </p>
   </div>
 </template>
 
@@ -23,5 +26,8 @@ export default {
 </script>
 
 <style lang="scss">
-
+#visualization {
+  margin: 2.5%;
+  width: 95%;
+}
 </style>
