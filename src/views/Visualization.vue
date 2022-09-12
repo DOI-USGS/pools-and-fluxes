b01b7396fbf68b8e915bf8afae5e64b4f0ee2417
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-09-12 16:50:07 -0500

    edit data statement

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -3,7 +3,7 @@
     <h2>Pools and Fluxes in the Water Cycle</h2>
     <Logpile />
     <p>
-      This visualization was inspired by the <a href="https://www.duncangeere.com/carbonincontext/" target="_blank">Carbon in Context</a> logpile chart by Duncan Geere. The magnitude of water cycle components are from <a href="https://www.nature.com/articles/s41561-019-0374-y" target="_blank">Abbott et al. (2019) Human domination of the global water cycle absent from depictions and perceptions.</a>  
+      This visualization was inspired by the <a href="https://www.duncangeere.com/carbonincontext/" target="_blank">Carbon in Context</a> logpile chart by Duncan Geere. Data are adapted from <a href="https://www.nature.com/articles/s41561-019-0374-y" target="_blank">Abbott et al. (2019) Human domination of the global water cycle absent from depictions and perceptions.</a>  
     </p>
   </div>
 </template>
