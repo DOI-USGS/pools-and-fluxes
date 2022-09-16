faeb4c3c84a8d7331dd8808a566c2321caebfe40
Cee Nell <cnell@usgs.gov>
2022-09-16 14:44:05 -0500

    combine in data to single file

--- _targets.R
+++ _targets.R
@@ -1,40 +1,30 @@
 library(targets)
-library(dssecrets)
 library(tidyverse)
 
 options(tidyverse.quiet = TRUE)
+tar_option_set(packages = c('readxl'))
 
 source("data-src/s3_upload.R")
 source("data-src/prep_logpile_data.R")
 
 list(
   tar_target(
-    abbott_data,
-    # magnitude of pools and fluxes
-    read_csv('in/abbott-pools-and-fluxes.csv',
-             skip = 1,
-             col_types = c('ccccnnn'),
-             col_names = c('Type','Category','Feature_group','Feature','Vol_1000km3','Vol_km3', "Vol_m3"))
+    # data as xlsx so manually editable
+    water_volume_xlsx,
+    # magnitude of pools and fluxes and filepaths/credits for images
+    'in/abbott-pools-and-fluxes.xlsx'
   ),
   tar_target(
-    image_data,
-    # contains image file names that correspond to pools and fluxes.
-    # derived from sheet Bekah is working on, so keeping separate from main data
-    read_csv('in/abbott-pools-and-fluxes_images.csv',
-             skip = 1,
-             col_types = c('cccccc'),
-             col_names = c('Type','Category','Feature_group','Feature',"image_file", "image_credit"))
+    # convert to csv to read into javascript page - should be json??
+    water_volume_csv,
+    read_xlsx(water_volume_xlsx) |> write_csv('out/water_volume_pools_and_fluxes.csv')
   ),
   tar_target(
-    water_volume_logpile_csv,
-    prep_logpile_data(abbott_data, image_data, file_out = 'out/water_volume_logpile.csv'),
-    format = "file"
-  ),
-  tar_target(
-    water_volume_logpile_log,
-    s3_upload(filepath_s3 = "visualizations/data/abbott_pools_and_fluxes.csv",
+    # push file to prod on s3 so accessible by vue
+    water_volume_s3,
+    s3_upload(filepath_s3 = "visualizations/data/water_volume_pools_and_fluxes.csv",
               on_exists = "replace",
-              filepath_local = water_volume_logpile_csv
+              filepath_local = water_volume_xlsx
   )),
   tar_target(
     # images to upload to s3
