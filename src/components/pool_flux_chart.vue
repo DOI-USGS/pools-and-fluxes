bdbf456d65390bed5a9d972fa89a4e5c82312548
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-09-22 18:21:23 -0500

    Color and bold title

--- src/components/pool_flux_chart.vue
+++ src/components/pool_flux_chart.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <div id="page-content">
-      <h1>Pools and fluxes in the water cycle</h1>
+      <h1><span class='poolText' >Pools</span> and <span class='fluxText'>fluxes</span> in the water cycle</h1>
       <p><span class='poolText' >Pools</span> are places where water is stored, like the ocean. <span class='fluxText'>Fluxes</span> are the ways that water moves between pools, such as evaporation, precipitation, discharge, recharge, or human use.</p>
       <p>Learn more about the water cycle and see the water cycle diagram on the <a href="https://www.usgs.gov/water-cycle" target="_blank">USGS Water Science School website.</a></p>
       <h3>Click on any row of the chart to pull up more information</h3>
