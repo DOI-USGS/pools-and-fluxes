c6e8b896ac3da8e428916ba539ded05cc85c40bd
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-10-21 15:53:22 -0500

    Add aria labels and roles to dialog elements

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -1,36 +1,35 @@
 <template>
-  <div v-show="show" class="overlay">
+  <div v-show="show" class="overlay" role="none presentation">
 
-
-    <div class="dialog" role="dialog">
+    <div class="dialog" role="dialog" aria-labelledby="dialogTitle">
       <header class="dialog__header" :style="{'background-color': color}">
         <h3 class="dialog__type" v-text="type"></h3>
       </header>
 
-      <hr class="dialog__break"/>
+      <hr class="dialog__break" />
 
       <section class="dialog__content">
-        <h2 class="dialog__title" v-text="title"></h2>
+        <h2 id="dialogTitle" class="dialog__title" v-text="title"></h2>
         <p><span class="dialog__size_prefix" v-text="sizePrefix"></span>
-        <span class="dialog__size"><a v-bind:href="dataSource" target="_blank" v-text="size"></a></span></p>
+        <span class="dialog__size"><a role="link" v-bind:href="dataSource" target="_blank" v-text="size"></a></span></p>
         <p class="dialog__range" v-text="range"></p>
         <p class="dialog__def_prefix" v-text="definitionPrefix"></p>
         <div class="dialog__def_content">
           <picture>
-            <source v-bind:srcset="sourceWebp" type="image/webp">
-            <source v-bind:srcset="source" type="image/png">
-            <a v-bind:href="imageSite" target="_blank">
-            <img class="dialog__image" v-bind:src="sourceWebp" width="200px" height="200px" v-bind:alt="altText">
+            <source v-bind:srcset="sourceWebp" type="image/webp" role="none presentation">
+            <source v-bind:srcset="source" type="image/png" role="none presentation">
+            <a role="link" aria-label="link to image source" v-bind:href="imageSite" target="_blank">
+            <img class="dialog__image" role="image" v-bind:src="sourceWebp" width="200px" height="200px" v-bind:alt="altText">
             </a>
           </picture>
-          <p class="dialog__definition" v-text="definition"></p>
+          <p class="dialog__definition" v-text="definition" aria-label="definition"></p>
         </div>
       </section>
     
-      <hr class="dialog__break"/>
+      <hr class="dialog__break" role="separator" />
 
       <footer class="dialog__footer">
-        <button @click="close" class="button dialog__close" role="button"><p>Close</p></button>
+        <button @click="close" class="button dialog__close" ><p>Close</p></button>
       </footer>
 
     </div>
