c1ff0931dfa0e702b01af64d7e4f4582f8c8b749
Cee <cnell@usgs.gov>
2026-03-04 08:50:04 -0600

    adjust dialog text and spacing

--- src/components/DialogBox.vue
+++ src/components/DialogBox.vue
@@ -22,8 +22,6 @@
         />
       </header>
 
-      <hr class="dialog__break">
-
       <section class="dialog__content">
         <h2
           id="dialogTitle"
@@ -133,10 +131,11 @@ export default {
   margin-left: auto;
   margin-right: auto;
   margin-top: 2.5rem;
-  width: 48rem;
-  max-width: 94%;
+  width: 56rem;
+  max-width: 96%;
   max-height: 90%;
-  font-size: 14px;
+  font-size: 16px;
+  line-height: 1.4;
 }
 .dialog__break {
   border: 0.5px solid white;
@@ -147,43 +146,40 @@ export default {
   padding-left: 0.75rem;
   padding-right: 0.75rem;
   padding-top: 0.25rem;
-  padding-bottom: 0.25rem;
+  padding-bottom: 0.5rem;
 }
 .dialog__content {
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
   padding-top: 1rem;
   padding-bottom: 1rem;
 }
 .dialog__title {
-  font-size: 1.5em;
+  font-size: 2em;
   font-weight: 700;
-  line-height: 1.75rem;
-  margin-bottom: 0.5rem;
+  line-height: 1.25;
+  margin-top: 0.5rem;
+  margin-bottom: 0.65rem;
+  padding-top: 0;
 }
 .dialog__type {
-  font-size: 1.17em;
+  font-size: 1.5em;
   font-weight: 500;
-  line-height: 1.75rem;
+  line-height: 1.25;
   margin-bottom: 0.5rem;
 }
 .dialog__size_prefix {
   font-weight: 700;
 }
 .dialog__size {
-  line-height: 1rem;
+  line-height: 1.4;
   font-weight: 500;
-  padding: 0.25em 0 0 0;
+  padding: 0;
 }
 .dialog__range {
-  line-height: 1rem;
+  line-height: 1.2;
   margin-bottom: 1rem;
-  padding: 0.25em 0 0 0;
-  color: #6E6E6E;
-}
-.dialog__range {
-  line-height: 1rem;
-  margin-bottom: 1rem;
+  padding: 0;
   color: #6E6E6E;
 }
 .dialog__image {
@@ -197,7 +193,7 @@ export default {
   min-height: 200px;
 }
 .dialog__definition {
-  line-height: 1.25rem;
+  line-height: 1.45;
   margin-bottom: 0.1rem;
 }
 .dialog__footer {
